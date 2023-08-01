3ec2b16591ce39ecd41e964f4e036663e6fe98ea
agneta1991 <pupienytea@gmail.com>
2023-08-01 12:27:15 +0300

    new app API created

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import './style.css';
 import fetchEpisodes from './modules/api.js';
 import displayEpisodesData from './modules/displaying-data.js';
 import seeMoreBtn from './modules/eventListeners.js';
+import createNewApp from './modules/involvementAPI.js';
 
 (async () => {
   const displayData = await fetchEpisodes();
   displayEpisodesData(displayData);
   seeMoreBtn();
+  createNewApp();
 })();
