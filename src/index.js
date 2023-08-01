59715b586fd33143ecd98926078412d0de9e2471
agneta1991 <pupienytea@gmail.com>
2023-08-01 11:20:07 +0300

    api fetched & main container displayed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css';
+import fetchEpisodes from './modules/api.js';
+import { displayEpisodesData } from './modules/displaying-data.js';
+import { seeMoreBtn } from './modules/eventListeners.js';
+
+(async () => {
+    const displayData = await fetchEpisodes();
+    displayEpisodesData(displayData);
+    seeMoreBtn();
+})();
+
---
+++ src/modules/displaying-data.js
@@ -0,0 +1,35 @@
+export function displayEpisodesData(displayData) {
+    const episodesSection = document.getElementById('main-container');
+
+    displayData.forEach((individualEpisode, index) => {
+        const div = document.createElement('div');
+        div.className = 'episodes-div';
+
+        if (index >= 6) {
+            div.classList.add('hidden');
+        }
+
+        const imageElement = document.createElement('img');
+        imageElement.src = individualEpisode.image.medium;
+        imageElement.alt = individualEpisode.name;
+
+        const pElement = document.createElement('p');
+        // instead of word rating, add icon that Winni found
+        pElement.textContent = `${individualEpisode.name} - Rating: ${individualEpisode.rating.average}`;
+
+        const commentsBtn = document.createElement('button');
+        commentsBtn.className = 'comments';
+        commentsBtn.innerHTML = 'Comments';
+
+        div.appendChild(imageElement);
+        div.appendChild(pElement);
+        div.appendChild(commentsBtn);
+
+        episodesSection.appendChild(div);
+    });
+
+    const seeMore = document.createElement('button');
+    seeMore.id = 'see-more';
+    seeMore.innerHTML = 'SEE MORE';
+    episodesSection.appendChild(seeMore);
+}
