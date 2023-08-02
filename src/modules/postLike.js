04e8801a31ceb895aa458df774860da2e10f5ea4
agneta1991 <pupienytea@gmail.com>
2023-08-02 12:55:14 +0300

    likes posted to server issue fixed

--- src/modules/postLike.js
+++ src/modules/postLike.js
@@ -3,7 +3,7 @@ import uniqueId from "..";
 const postLike = async (likes, event) => {
     const newAppId = await uniqueId();
   const item1Url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${newAppId}/likes`;  
-  const item1 = event.target.parentElement.clasName;
+  const item1 = event.target.parentElement.className;
   const requestData = {
     method: 'POST',
     headers: {
@@ -15,10 +15,9 @@ const postLike = async (likes, event) => {
     }),
   };
 
-  const response = await fetch(item1Url, requestData);
-  console.log(response);
-  const responseData = await response.json();
-  console.log('Response:', responseData);
+    const response = await fetch(item1Url, requestData);
+    console.log (response);
+
 };
 
 export default postLike;
