02576d19c7c0076e26888c192d5385384e00d1d6
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-02 09:25:11 +0100

    Image issue

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import Logo from './color-no-bg.png';
+// import Logo from './color-no-bg.png';
 import fetchEpisodes from './modules/api.js';
 import displayEpisodesData from './modules/displaying-data.js';
 import seeMoreBtn from './modules/eventListeners.js';
@@ -10,6 +10,6 @@ import seeMoreBtn from './modules/eventListeners.js';
   seeMoreBtn();
 })();
 
-const headerLogo = document.querySelector('#logo');
-headerLogo.className = 'logo-img';
-headerLogo.src = Logo;
+// const headerLogo = document.querySelector('#logo');
+// headerLogo.className = 'logo-img';
+// headerLogo.src = Logo;
