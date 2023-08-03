0d07e8bec480ee6fd09d3f05a1335dbc284b965f
agneta1991 <pupienytea@gmail.com>
2023-08-03 17:42:46 +0300

    linters fixed

--- src/modules/commentsCounter.js
+++ src/modules/commentsCounter.js
@@ -3,13 +3,13 @@ import { getComments } from './commentApi.js';
 const commentsCounter = async (itemId) => {
   const commentArr = await getComments(itemId);
   const commentCount = commentArr.length;
-if (commentCount === undefined) {
-  let countSpan = document.querySelector('.comment-counter');
-  countSpan.innerHTML = '0'
-} else {
- let countSpan = document.querySelector('.comment-counter');
-  countSpan.innerHTML = commentCount;
-}
+  if (commentCount === undefined) {
+    const countSpan = document.querySelector('.comment-counter');
+    countSpan.innerHTML = '0';
+  } else {
+    const countSpan = document.querySelector('.comment-counter');
+    countSpan.innerHTML = commentCount;
+  }
 
   return commentCount;
 };
